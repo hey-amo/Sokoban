7d19b508d7ca509ba35c814f15f9f1d62557a8f6
AD <silentflute.ad@gmail.com>
2026-07-25 12:28:04 +0100

    initial project build = xcode

---
+++ Sokoban/Sokoban/SokobanApp.swift
@@ -0,0 +1,17 @@
+//
+//  SokobanApp.swift
+//  Sokoban
+//
+//  Created by Amarjit on 25/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct SokobanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
